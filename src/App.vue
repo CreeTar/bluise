f43375adcdcf1626053c2bc26b72a6fc7954e814
aschikora@hotmail.de <aschikora@hotmail.de>
2022-01-15 01:40:11 +0100

    -

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld />
   </div>
 </template>
 
@@ -21,8 +20,13 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  font-family: 'Open Sans', sans-serif;
+  line-height: 1.75em;
+  font-size: 16px;
+  background-color: #222;
+  color: #aaa;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,128 @@
 <template>
-  <div class="hello">
+  <div>
+    <table>
+      <tr>
+        <td valign="center"><img src="https://crypto.com/exchange/img/logo.76653258.svg" width="169" height="34" alt="cryto.com"/></td>
+        <td valign="center" style="font-weight: normal; color: #e4e4e4; padding-bottom: 8px; font-size: 14pt; font-family: Verdana;">- CSV-Parser</td>
+      </tr>
+      <tr>
+        <td colspan="2"><hr size="1">
+        </td>
+      </tr>
+      <tr style="font-size: 10pt">
+        <td>
+<!--          <button @click="$refs['uploadCtr'].click()">Select crypto_transactions_records...csv</button>-->
+          <input type="file" id="uploadCtr" multiple style="display: block;" ref="uploadCtr">
+        </td>
+        <td><button @click="checkUpload">upload</button></td>
+      </tr>
+    </table>
     <h1>{{ getPrice }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
+    <table>
+      <tr>
+        <th colspan="2">Supercharger Rewards</th>
+      </tr>
+      <tr style="font-family: Consolas; font-size: 11pt;">
+        <th>Coin</th>
+        <th>Amount</th>
+      </tr>
+      <tr v-for="asset in Object.keys(superChargerRewards)" style="font-family: Consolas; font-size: 10pt;">
+        <td>{{ asset }}</td>
+        <td>{{ superChargerRewards[asset] }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      superChargerRewards: {},
+      tickerData: {}
+    }
+  },
+  created() {
+    fetch('https://api.crypto.com/v2/public/get-ticker').then((response) => {
+      this.tickerData = response.json();
+    });
+  },
+  methods: {
+    getRate(asset) {
+      // find USDT rate frist
+      const usdtRate = this.tickerData.result.data.find((ticker) => ticker['i'] === asset + "_USDT");
+      if (usdtRate) {
+        return Number(usdtRate['b']);
+      }
+
+      console.warn('no USDT rate, checking for BTC');
+      // find BTC rate
+      const btcRate = this.tickerData.result.data.find((ticker) => ticker['i'] === asset + "_BTC");
+      if (btcRate) {
+        const btcRate = Number(btcRate['b']);
+        const btcUsdtRate = this.tickerData.result.data.find((ticker) => ticker['i'] === "BTC_USDT");
+        return btcRate * Number(btcUsdtRate)
+      }
+
+      console.error('No rate for BTC or USDT was found')
+      return 0
+    },
+    checkUpload() {
+      const files = this.$refs['uploadCtr'].files;
+      for (let file of files) {
+        console.log(file);
+        if (file) {
+          if (file.name.startsWith('crypto_transactions_record')) {
+            this.parseCryptoTransactionsRecords(file);
+          } else {
+            console.warn('unkown file type');
+          }
+        } else {
+          console.debug("no file selected");
+        }
+      }
+    },
+    async getFileContent(file) {
+      return await file.text();
+    },
+    async parseCryptoTransactionsRecords(file) {
+      const content = (await this.getFileContent(file)).toString();
+      const lines = content.split('\n')
+      const header = lines.shift();
+      const entries = lines.map((line) => line.split(','))
+
+      const idxDesc = 1;
+      const idxAsset = 2;
+      const idxAmount = 3;
+      for (const entry of entries) {
+        if (entry[idxDesc] === 'Supercharger Reward') {
+          console.log(entry.join('|'))
+          const asset = entry[idxAsset];
+          const currentAmount = this.superChargerRewards[asset];
+          let newAmount = Number(entry[idxAmount]);
+          if (currentAmount) {
+            newAmount += Number(currentAmount)
+          }
+
+          this.$set(this.superChargerRewards, asset, newAmount)
+        }
+      }
+
+      // console.log(entries);
+    }
   },
   computed: {
-    getPrice: async () => {
-      return (await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=EUR')).json();
+    getPrice: () => {
+      return 31234.56;
+      //(await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=EUR')).json().toString();
     }
   }
 }
 </script>
 
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+th { text-align: left; }
 a {
   color: #42b983;
 }
