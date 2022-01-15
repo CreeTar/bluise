01e3d59bdc054caddeec288e415bf13a86b1e5a4
aschikora@hotmail.de <aschikora@hotmail.de>
2022-01-15 17:36:01 +0100

    update function

--- netlify/functions/node-fetch.js
+++ netlify/functions/node-fetch.js
@@ -9,19 +9,17 @@ const handler = async function () {
       // NOT res.status >= 200 && res.status < 300
       return { statusCode: response.status, body: response.statusText }
     }
-    const data = await response.json()
+    const json = await response.json()
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ msg: data.joke }),
+      body: JSON.stringify(json.data),
     }
   } catch (error) {
-    // output to netlify function log
     console.log(error)
     return {
       statusCode: 500,
-      // Could be a custom message or object i.e. JSON.stringify(err)
-      body: JSON.stringify({ msg: error.message }),
+      body: JSON.stringify({ msg: error.code }),
     }
   }
 }
